bdec42c051018781bb2b11345308e42a7d4fb071
Hung Do <90897838+Hungumn@users.noreply.github.com>
2023-06-15 12:14:10 +0700

    code base

---
+++ utils/data-mock.js
@@ -0,0 +1,70 @@
+export const Data = [
+	{
+		id: 1,
+		title: 'Introduction ReactJs',
+		description: 'What is React, Virtual Dom, How to make a React App',
+		link: '',
+	},
+	{
+		id: 2,
+		title: 'ReactJs Components',
+		description: 'What is Component, Class Component, Function Component, Higher Order Component',
+		link: '/Component-react',
+	},
+	{ id: 3, title: 'JSX Element', description: 'What is JSX, Converting HTML to JSX, The Rules of JSX', link: '' },
+	{
+		id: 4,
+		title: 'Event Handling in React',
+		description: 'Adding event handlers, Basic Syntax of ReactJs event handling, Hook',
+		link: '',
+	},
+	{
+		id: 5,
+		title: 'ReactJs Router',
+		description:
+			'What is React Router, SSP and MPA, Basic Component of React Router, Router in NextJs, Get params in router with NextJs',
+		link: '',
+	},
+	{
+		id: 6,
+		title: 'Styling React Element',
+		description: 'Styling CSS: Inline, Internal and External; How to make CSS with JSX',
+		link: '',
+	},
+	{ id: 7, title: 'ReactJs Component LifeCycle', description: '', link: '' },
+	{ id: 8, title: 'Creating a CRUD Application using React Form', description: '', link: '' },
+	{ id: 9, title: 'Example Todo App', description: '', link: '' },
+];
+
+export const DataComponent = [
+	{
+		id: 1,
+		title: 'Component 1',
+		images: './images/1.jpg',
+	},
+	{
+		id: 2,
+		title: 'Component 2',
+		images: './images/2.jpg',
+	},
+	{
+		id: 3,
+		title: 'Component 3',
+		images: './images/3.jpg',
+	},
+	{
+		id: 4,
+		title: 'Component 4',
+		images: './images/4.jpg',
+	},
+	{
+		id: 5,
+		title: 'Component 5',
+		images: './images/5.jpg',
+	},
+	{
+		id: 6,
+		title: 'Component 6',
+		images: './images/6.jpg',
+	},
+];
